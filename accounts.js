57713db85859ec61197a3c0f0e54fa98a3f13aad
macman000 <macmellors@gmail.com>
2026-05-30 13:08:05 -0700

    Update passwords for existing accounts

--- accounts.js
+++ accounts.js
@@ -3,8 +3,8 @@
 // To add a member: copy one of the lines below and fill in their name and password
 // ============================================================
 const ACCOUNTS = [
-  { name: 'Mac M.',    password: 'PASSWORD_MAC_HERE',    role: 'owner'  },
-  { name: 'Minnie M.', password: 'PASSWORD_MINNIE_HERE', role: 'owner'  },
+  { name: 'Mac M.',    password: 'Iammac5820',    role: 'owner'  },
+  { name: 'Minnie M.', password: 'rocks', role: 'owner'  },
   // Add members below:
   { name: 'Jorie Mellors',  password: 'Boogerbooger', role: 'member' },
   { name: 'Nicholas Mellors',   password: '987654321', role: 'member' },
